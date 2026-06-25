8c52e82e6886750157908b07aa7d8b66a1368b22
Linn <worldof.s@yandex.ru>
2026-06-25 11:47:35 +0500

    Добавлена база данных PostgreSQL

---
+++ Models/Feedback.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CleaningRequests.Models;
+
+public partial class Feedback
+{
+    public int Id { get; set; }
+
+    public int Stars { get; set; }
+
+    public string? Comment { get; set; }
+
+    public int Request_id { get; set; }
+
+    public virtual Request Request { get; set; } = null!;
+}
---
+++ Models/Request.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CleaningRequests.Models;
+
+public partial class Request
+{
+    public int Id { get; set; }
+
+    public string Full_name { get; set; } = null!;
+
+    public string Phone { get; set; } = null!;
+
+    public string Address { get; set; } = null!;
+
+    public string Cabinets { get; set; } = null!;
+
+    public DateOnly Request_date { get; set; }
+
+    public TimeOnly Request_time { get; set; }
+
+    public string? Comment { get; set; }
+
+    public int Status_id { get; set; }
+
+    public virtual ICollection<Feedback> Feedbacks { get; set; } = new List<Feedback>();
+
+    public virtual Status Status { get; set; } = null!;
+
+    public virtual ICollection<Service> Services { get; set; } = new List<Service>();
+}
